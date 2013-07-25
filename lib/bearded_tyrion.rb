feb5c69e1e7bb51647a8be7d721f3886f25a0516
Andrew8xx8 <avk@8xx8.ru>
2013-07-25 11:23:27 +0400

    Refactored

--- lib/bearded_tyrion.rb
+++ lib/bearded_tyrion.rb
@@ -6,5 +6,6 @@ require 'haml'
 
 module BeardedTyrion
   autoload 'Web', File.expand_path('../bearded_tyrion/web', __FILE__)
+  autoload 'Twitter', File.expand_path('../bearded_tyrion/twitter', __FILE__)
   autoload 'User', File.expand_path('../bearded_tyrion/user', __FILE__)
 end
--- logic.rb
+++ logic.rb
@@ -1,26 +1,3 @@
-def consumer_key
-  raise "Missing CONSUMER_KEY in ENV!" unless ENV["CONSUMER_KEY"]
-
-  ENV["CONSUMER_KEY"]
-end
-
-def consumer_secret
-  raise "Missing CONSUMER_SECRET in ENV!" unless ENV["CONSUMER_SECRET"]
-
-  ENV["CONSUMER_SECRET"]
-end
-
-def oauth_token
-  return nil unless session.has_key?( :auth ) && session[:auth].has_key?( :token )
-
-  session[:auth][:token]
-end
-
-def oauth_token_secret
-  return nil unless session.has_key?( :auth ) && session[:auth].has_key?( :secret )
-
-  session[:auth][:secret]
-end
 
 def parse_omniauth(omniauth_hash)
   {
